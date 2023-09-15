8849279b226361a1206794acd30f9aa374d4eaba
ATOIsus <sabinmaharjan884@gmail.com>
2023-09-15 21:38:24 +0545

    Only required http request kept.

--- sabin/API/routes/supplier-routes.js
+++ sabin/API/routes/supplier-routes.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const supplierController = require("../controllers/supplier-controllers");
+const supplierController = require("../controllers/supplier-controller");
 
 router
-  .route("/")
+  .route("/dashboard")
   .get(supplierController.createUser)
 
-  .post((req, res, next) => {
-    res.status(405).json({ error: "POST request is not allowed!" });
-  })
-
-  .put((req, res, next) => {
-    res.status(405).json({ error: "PUT request is not allowed!" });
-  })
-
-  .delete((req, res, next) => {
-    res.status(405).json({ error: "DELETE request is not allowed!" });
-  });
 
 module.exports = router;
--- sabin/API/routes/user-routes.js
+++ sabin/API/routes/user-routes.js
@@ -1,85 +1,27 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/user-controller");
 
 
+router
+  .route("/signup")
+  //Add new user.
+  .post(userController.signUpUser);
 
-router.route('/')
-    .get(userController.getAllUsers)
+router
+  .route("/signin")
+  // User login.
+  .post(userController.signInUser);
 
-    .post((req, res, next) => {
-        res.status(405).json({ error: "POST request is not allowed!" })
-    })
+router
+  .route("/info")
+  //Get information of specific user.
+  .get(userController.getSingleUser);
 
-    .put((req, res, next) => {
-        res.status(405).json({ error: "PUT request is not allowed!" })
-    })
+router
+  .route("/requestwater")
+  //Request water.
+  .post(userController.requestWater);
 
-    .delete((req, res, next) => {
-        res.status(405).json({ error: "DELETE request is not allowed!" })
-    })
-
-router.route('/signup')
-    //Add new user.
-    .post(userController.signUpUser)
-
-    .get((req, res, next) => {
-        res.status(405).json({ error: "GET request is not allowed!" })
-    })
-
-    .put((req, res, next) => {
-        res.status(405).json({ error: "PUT request is not allowed!" })
-    })
-
-    .delete((req, res, next) => {
-        res.status(405).json({ error: "DELETE request is not allowed!" })
-    })
-
-
-router.route('/signin')
-    // User login (for both user and admin).
-    .post(userController.signInUser)
-
-    .get((req, res, next) => {
-        res.status(405).json({ error: "GET request is not allowed!" })
-    })
-
-    .put((req, res, next) => {
-        res.status(405).json({ error: "PUT request is not allowed!" })
-    })
-
-    .delete((req, res, next) => {
-        res.status(405).json({ error: "DELETE request is not allowed!" })
-    })
-
-
-router.route('/info')
-    //Get, update and delete information of specific user.
-    .get(verifyUser, userController.getSingleUser)
-
-    .put(verifyUser, userController.updateUserDetails)
-
-    .delete(verifyUser, userController.deleteUser)
-
-    .post((req, res, next) => {
-        res.status(405).json({ error: "POST request is not allowed!" })
-    })
-
-
-router.route('/requestwater')
-    //Update password.
-    .post(verifyUser, userController.requestWater)
-
-    .get((req, res, next) => {
-        res.status(405).json({ error: "GET request is not allowed!" })
-    })
-
-    .put((req, res, next) => {
-        res.status(405).json({ error: "PUT request is not allowed!" })
-    })
-
-    .delete((req, res, next) => {
-        res.status(405).json({ error: "DELETE request is not allowed!" })
-    })
-
-
-module.exports = router
+module.exports = router;
